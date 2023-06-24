e70171dc4af8acd620b7ca4612d13cfed00d45a1
kvqn <guneetaggarwal@gmail.com>
2023-06-24 16:53:23 +0530

    initial commit

---
+++ a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+void solve() {
+
+  print("hello")
+
+}
+
+
+int main () {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+}
